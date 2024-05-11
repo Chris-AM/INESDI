9d416b67b3df9526b5fe418e77a24bdb50735c9b
Chris-AM <c.aranguizm@outlook.com>
2024-05-11 02:28:22 -0400

    :pencil2: fixing return contract types

--- 0_bootcamp/minimarket_api/src/features/products/domain/product.datasource.ts
+++ 0_bootcamp/minimarket_api/src/features/products/domain/product.datasource.ts
@@ -5,26 +5,26 @@
 // puede realizar un usuario manipulando la app
 
 import { Observable } from 'rxjs';
-import { Product } from './schema/product.schema';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductDocument } from './domain';
 
 export interface ProductDatasource {
   // Verbo para comprar productos
   // Recibe un producto (DTO), del cual crea una instancia y retorna un observable de productos
   // Será verbo POST
-  comprarProductos(product: CreateProductDto): Observable<Product[]>;
+  comprarProductos(product: CreateProductDto): Observable<ProductDocument[]>;
   // Verbo para obtener productos
   // Retorna un observable de productos
   // Será verbo GET
-  obtenerProductos(): Observable<Product[]>;
+  obtenerProductos(): Observable<ProductDocument[]>;
   // Verbo para obtener un producto por algún criterio de búsqueda
   // Se le pasa un término de búsqueda y retorna un observable de un producto
   // Será verbo GET
-  obtenerProducto(searchTerm: string): Observable<Product>;
+  obtenerProducto(searchTerm: string): Observable<ProductDocument>;
   // Verbo para obtener un producto por id y luego venderlo
   // Recibe un producto (DTO), del cual descrimina por codigo de barras
   // y retorna un observable de productos
   // Será verbo PATCH y actualizará el stock del producto
-  venderProducto(product: UpdateProductDto): Observable<Product>;
+  venderProducto(product: UpdateProductDto): Observable<ProductDocument>;
 }
