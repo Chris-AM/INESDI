79004f485d01630896ca973374b32e58f8624535
Chris-AM <c.aranguizm@outlook.com>
2024-05-11 00:20:48 -0400

    :building_construction: declaring products infrastructure layer

--- 0_bootcamp/minimarket_api/src/features/products/infrastructure/product.datasource.impl.ts
+++ 0_bootcamp/minimarket_api/src/features/products/infrastructure/product.datasource.impl.ts
@@ -1,115 +1,41 @@
 import { Model } from 'mongoose';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 //Own Imports
 import {
   CreateProductDto,
   Product,
   ProductDatasource,
-  ProductDocument,
+  ProductDocumen,
+  ProductImage,
   UpdateProductDto,
 } from '../domain/domain';
-import { LoggerService } from 'src/config/logger/logger.service';
 
 @Injectable()
 export class ProductDatasourceImpl implements ProductDatasource {
   constructor(
     @InjectModel(Product.name)
-    private readonly productModel: Model<ProductDocument>,
-    private readonly loggerService: LoggerService,
+    private readonly productModel: Model<ProductDocumen>,
+    @InjectModel(ProductImage.name)
+    private readonly productImageModel: Model<ProductImage>,
   ) {}
-
-  async comprarProductos(
-    product: CreateProductDto,
-  ): Promise<ProductDocument[]> {
+  comprarProductos(product: CreateProductDto): Observable<Product[]> {
     // Deconstruye el producto
-    const { codeBar, stock } = product;
-    const productsToBuy: ProductDocument[] = [];
-    try {
-      // Buscar el producto por el código de barras
-      const obtainedProduct = await this.obtenerProducto(codeBar);
-      // Si el producto no existe, se crea
-      if (!obtainedProduct) {
-        const createdProduct = await this.createProduct(product);
-        productsToBuy.push(createdProduct);
-      } else {
-        // Si el producto existe, se actualiza el stock
-        obtainedProduct.stock += stock;
-        await obtainedProduct.save();
-        productsToBuy.push(obtainedProduct);
-      }
-      return productsToBuy;
-    } catch (error) {
-      this.loggerService.error(
-        error.message,
-        error.stack,
-        'ProductDatasourceImpl',
-      );
-    }
+    const { codeBar, name, stock, buyPrice, minStock } = product;
+    // Revisar si el producto ya existe
+    const obtainedProduct = this.obtenerProducto(codeBar);
+    throw new Error('Method not implemented.');
   }
-
-  async obtenerProductos(): Promise<ProductDocument[]> {
-    const allProducts = await this.productModel.find();
-    this.loggerService.log('ProductDatasourceImpl', 'Products Obtained');
-    return allProducts;
+  obtenerProductos(): Observable<Product[]> {
+    throw new Error('Method not implemented.');
   }
-
-  async obtenerProducto(searchTerm: string): Promise<ProductDocument> {
+  obtenerProducto(searchTerm: string): Observable<Product> {
     // Buscar el producto por el código de barras
-    try {
-      const product = await this.productModel.findOne({ codeBar: searchTerm });
-      this.loggerService.log('ProductDatasourceImpl', 'Product Obtained');
-      return product;
-    } catch (error) {
-      this.loggerService.error(
-        error.message,
-        error.stack,
-        'ProductDatasourceImpl',
-      );
-    }
+    const product = this.productModel.findOne({ codeBar: searchTerm });
+    throw new Error('Method not implemented.');
   }
-
-  async venderProducto(
-    searchedCodeBar: string,
-    quantity: number,
-  ): Promise<ProductDocument> {
-    try {
-      const obtainedProduct = await this.obtenerProducto(searchedCodeBar);
-      if (!obtainedProduct) {
-        throw new NotFoundException('Product not found');
-      }
-      const newStockProduct = (obtainedProduct.stock -= quantity);
-      obtainedProduct.stock = newStockProduct;
-      return obtainedProduct.save();
-    } catch (error) {
-      this.loggerService.error(
-        error.message,
-        error.stack,
-        'ProductDatasourceImpl',
-      );
-    }
-  }
-
-  //* Delete method is not at datasource
-  //* level because it's not used by the service
-  public async deleteBySeed(): Promise<void> {
-    const query = this.productModel.deleteMany({});
-    await query.exec();
-  }
-
-  private async createProduct(
-    product: CreateProductDto,
-  ): Promise<ProductDocument> {
-    try {
-      const createdProduct = await this.productModel.create(product);
-      this.loggerService.log('ProductDatasourceImpl', 'Product Created');
-      return createdProduct;
-    } catch (error) {
-      this.loggerService.error(
-        error.message,
-        error.stack,
-        'ProductDatasourceImpl',
-      );
-    }
+  venderProducto(product: UpdateProductDto): Observable<Product> {
+    throw new Error('Method not implemented.');
   }
 }
