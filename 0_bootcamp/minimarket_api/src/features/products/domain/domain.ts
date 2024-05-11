997e290b4823e57a0ebfb621148b293405cd6a10
Chris-AM <c.aranguizm@outlook.com>
2024-05-11 00:02:05 -0400

    :technologist: adding domain layer barrer roll

---
+++ 0_bootcamp/minimarket_api/src/features/products/domain/domain.ts
@@ -0,0 +1,4 @@
+export { ProductDatasource } from '../domain/product.datasource';
+export { CreateProductDto } from '../domain/dto/create-product.dto';
+export { UpdateProductDto } from '../domain/dto/update-product.dto';
+export { Product, ProductDocumen } from '../domain/schema/product.schema';
