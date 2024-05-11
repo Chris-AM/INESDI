d1e1e723ff56dc88a464acf2ff95ad5b6014c8de
Chris-AM <c.aranguizm@outlook.com>
2024-05-11 04:17:14 -0400

    :pencil2: fixing return contract types

--- 0_bootcamp/minimarket_api/src/features/products/domain/product.datasource.ts
+++ 0_bootcamp/minimarket_api/src/features/products/domain/product.datasource.ts
@@ -4,27 +4,26 @@
 // Pero realmente se declaran las acciones que
 // puede realizar un usuario manipulando la app
 
-import { Observable } from 'rxjs';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductDocument } from './domain';
 
 export interface ProductDatasource {
   // Verbo para comprar productos
-  // Recibe un producto (DTO), del cual crea una instancia y retorna un observable de productos
+  // Recibe un producto (DTO), del cual crea una instancia y retorna un Promise de productos
   // Será verbo POST
-  comprarProductos(product: CreateProductDto): Observable<ProductDocument[]>;
+  comprarProductos(product: CreateProductDto): Promise<ProductDocument[]>;
   // Verbo para obtener productos
-  // Retorna un observable de productos
+  // Retorna un Promise de productos
   // Será verbo GET
-  obtenerProductos(): Observable<ProductDocument[]>;
+  obtenerProductos(): Promise<ProductDocument[]>;
   // Verbo para obtener un producto por algún criterio de búsqueda
-  // Se le pasa un término de búsqueda y retorna un observable de un producto
+  // Se le pasa un término de búsqueda y retorna un Promise de un producto
   // Será verbo GET
-  obtenerProducto(searchTerm: string): Observable<ProductDocument>;
+  obtenerProducto(searchTerm: string): Promise<ProductDocument>;
   // Verbo para obtener un producto por id y luego venderlo
   // Recibe un producto (DTO), del cual descrimina por codigo de barras
-  // y retorna un observable de productos
+  // y retorna un Promise de productos
   // Será verbo PATCH y actualizará el stock del producto
-  venderProducto(product: UpdateProductDto): Observable<ProductDocument>;
+  venderProducto(product: UpdateProductDto): Promise<ProductDocument>;
 }
