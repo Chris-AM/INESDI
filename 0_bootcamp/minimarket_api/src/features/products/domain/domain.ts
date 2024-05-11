0d5a70ceb2f84a95d64bb31220defa3e2f9b0a3d
Chris-AM <c.aranguizm@outlook.com>
2024-05-11 00:03:58 -0400

    :technologist: exporting prod images

--- 0_bootcamp/minimarket_api/src/features/products/domain/domain.ts
+++ 0_bootcamp/minimarket_api/src/features/products/domain/domain.ts
@@ -1,7 +1,5 @@
 export { ProductDatasource } from '../domain/product.datasource';
 export { CreateProductDto } from '../domain/dto/create-product.dto';
 export { UpdateProductDto } from '../domain/dto/update-product.dto';
-export {
-  Product,
-  ProductDocumen,
-} from '../domain/schema/product.schema';
+export { ProductImage } from '../domain/schema/product-image.schema';
+export { Product, ProductDocumen } from '../domain/schema/product.schema';
