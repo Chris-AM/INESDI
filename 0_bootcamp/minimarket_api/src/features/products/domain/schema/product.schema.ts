3584116b68d6e17f0ed584299be84bbe3ee69ff2
Chris-AM <c.aranguizm@outlook.com>
2024-05-10 22:31:10 -0400

    :construction: declaring product schemas. Not final

--- 0_bootcamp/minimarket_api/src/features/products/domain/schema/product.schema.ts
+++ 0_bootcamp/minimarket_api/src/features/products/domain/schema/product.schema.ts
@@ -1,17 +1,14 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { v4 } from 'uuid';
+import { ProductImage } from './product-image.schema';
 
-export type ProductDocument = Product & Document;
+export type ProductSchema = Product & Document;
 
 @Schema({ timestamps: true })
 export class Product {
   @Prop({ unique: true, default: v4 })
   id: string;
-  @Prop({ required: true })
-  codeBar: string;
-  @Prop({ required: true })
-  provider: string;
   @Prop({ required: true })
   name: string;
   @Prop({ required: true })
@@ -21,22 +18,15 @@ export class Product {
   @Prop({ required: true })
   stock: number;
   @Prop({ required: true })
-  minStock: number;
-  @Prop({ required: true })
   description: string;
+  @Prop({ required: true })
+  image: string;
   @Prop({ required: true })
   category: string;
   @Prop({ required: true })
   brand: string;
   @Prop({ required: true })
   discount: number;
-  @Prop({ ref: 'ProductImage' })
-  images: string[];
+  @Prop({ required: true, ref: 'ProductImage' })
+  images: ProductImage[];
 }
-
-export const ProductSchema = SchemaFactory.createForClass(Product);
-
-ProductSchema.method('toJSON', function () {
-  const { _id, ...object } = this.toObject();
-  return object;
-});
