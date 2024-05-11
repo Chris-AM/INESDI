81b66e565cd57df323e0603e5d0eea6026e4c7eb
Chris-AM <c.aranguizm@outlook.com>
2024-05-10 22:32:24 -0400

    :sparkles: adding feature modules to app

--- 0_bootcamp/minimarket_api/src/app.module.ts
+++ 0_bootcamp/minimarket_api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvironmentConfigModule } from './config/config.module';
+import { MinimarketModule } from './features/minimarket.module';
 
 @Module({
-  imports: [EnvironmentConfigModule],
+  imports: [EnvironmentConfigModule, MinimarketModule],
   controllers: [AppController],
   providers: [AppService],
 })
