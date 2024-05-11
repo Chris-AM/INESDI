89e0ee2c6d72b4845d276c6e443bcfb14977dd46
Chris-AM <c.aranguizm@outlook.com>
2024-05-10 22:43:51 -0400

    :sparkles: Product Schema Ready

--- 0_bootcamp/minimarket_api/src/features/products/domain/schema/product.schema.ts
+++ 0_bootcamp/minimarket_api/src/features/products/domain/schema/product.schema.ts
@@ -1,30 +1,18 @@
-<<<<<<< HEAD
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { v4 } from 'uuid';
 
 export type ProductDocument = Product & Document;
-=======
-import { Prop, Schema } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-import { v4 } from 'uuid';
-import { ProductImage } from './product-image.schema';
-
-export type ProductSchema = Product & Document;
->>>>>>> 6289733 (:construction: declaring product schemas. Not final)
 
 @Schema({ timestamps: true })
 export class Product {
   @Prop({ unique: true, default: v4 })
   id: string;
   @Prop({ required: true })
-<<<<<<< HEAD
   codeBar: string;
   @Prop({ required: true })
   provider: string;
   @Prop({ required: true })
-=======
->>>>>>> 6289733 (:construction: declaring product schemas. Not final)
   name: string;
   @Prop({ required: true })
   sellPrice: number;
@@ -33,23 +21,15 @@ export class Product {
   @Prop({ required: true })
   stock: number;
   @Prop({ required: true })
-<<<<<<< HEAD
   minStock: number;
   @Prop({ required: true })
   description: string;
   @Prop({ required: true })
-=======
-  description: string;
-  @Prop({ required: true })
-  image: string;
-  @Prop({ required: true })
->>>>>>> 6289733 (:construction: declaring product schemas. Not final)
   category: string;
   @Prop({ required: true })
   brand: string;
   @Prop({ required: true })
   discount: number;
-<<<<<<< HEAD
   @Prop({ ref: 'ProductImage' })
   images: string[];
 }
@@ -60,8 +40,3 @@ ProductSchema.method('toJSON', function () {
   const { _id, ...object } = this.toObject();
   return object;
 });
-=======
-  @Prop({ required: true, ref: 'ProductImage' })
-  images: ProductImage[];
-}
->>>>>>> 6289733 (:construction: declaring product schemas. Not final)
