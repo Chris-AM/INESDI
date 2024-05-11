fe37cfa4df5d0ee4a0bcb2df97d528c3d6359ddc
Chris-AM <c.aranguizm@outlook.com>
2024-05-11 00:20:01 -0400

    :sparkles: declaring database connection for specific schema

--- 0_bootcamp/minimarket_api/src/features/products/products.module.ts
+++ 0_bootcamp/minimarket_api/src/features/products/products.module.ts
@@ -1,9 +1,43 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ProductsService } from './use-case/products.service';
 import { ProductsController } from './use-case/products.controller';
+import { ProductDatasourceImpl } from './infrastructure/product.datasource.impl';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './domain/schema/product.schema';
+import { ProductImage } from './domain/domain';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      // Import the Product schema and apply the mongoose-autopopulate plugin
+      {
+        name: Product.name,
+        useFactory: () =>
+          ProductSchema.plugin(require('mongoose-autopopulate')),
+      },
+      {
+        name: ProductImage.name,
+        useFactory: () =>
+          ProductSchema.plugin(require('mongoose-autopopulate')),
+      },
+      // Import the ProductImage schema and apply the mongoose-paginate-v2 plugin
+      {
+        name: ProductImage.name,
+        useFactory: () => ProductSchema.plugin(require('mongoose-paginate-v2')),
+      },
+      {
+        name: Product.name,
+        useFactory: () => ProductSchema.plugin(require('mongoose-paginate-v2')),
+      },
+    ]),
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, ProductDatasourceImpl],
 })
-export class ProductsModule {}
+export class ProductsModule {
+  configure(consumer: MiddlewareConsumer){
+    // consumer
+    // .apply(PaginationMiddleware)
+    // .forRoutes({ path: 'v1/minimarket/products', method: RequestMethod.GET })
+  }
+}
