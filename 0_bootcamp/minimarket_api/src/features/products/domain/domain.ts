16a80e281f24e91c434b4788e7e3b6d4281cf7d9
Chris-AM <c.aranguizm@outlook.com>
2024-05-12 22:37:54 -0400

    :sparkles: running product mock

--- 0_bootcamp/minimarket_api/src/features/products/domain/domain.ts
+++ 0_bootcamp/minimarket_api/src/features/products/domain/domain.ts
@@ -1,7 +1,6 @@
 export { ProductDatasource } from '../domain/product.datasource';
 export { CreateProductDto } from '../domain/dto/create-product.dto';
 export { UpdateProductDto } from '../domain/dto/update-product.dto';
-export { ProductImage } from '../domain/schema/product-image.schema';
 export {
   Product,
   ProductDocument,
