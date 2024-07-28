49f8ae823dfc580e7baa4b3c5bcd865cb735d4fc
Chris-AM <c.aranguizm@outlook.com>
2024-07-28 18:36:11 -0400

    :sparkles: Module 2 Passed

--- 2_methodology_and_management/actividad-01/src/class_b.ts
+++ 2_methodology_and_management/actividad-01/src/class_b.ts
@@ -1,9 +1,11 @@
 export class B {
   foo: string;
   bar: string;
+  ipsun: string;
 
   constructor(foo: string, bar: string) {
     this.foo = foo;
     this.bar = bar;
+    this.ipsun = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
   }
 }
