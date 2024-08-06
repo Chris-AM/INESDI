612b729c4f3461e0abf045905c930385131cfb53
Chris-AM <c.aranguizm@outlook.com>
2024-08-06 00:09:41 -0400

    :sparkles: adding jobs to list

--- 3_front-end_intro/final_project/src/js/services/select-job.js
+++ 3_front-end_intro/final_project/src/js/services/select-job.js
@@ -1,4 +1,4 @@
-import { beDev1, beDev2, beDev3, dba1, dba2, dba3, devSecOp1, devSecOp2, devSecOp3, feDev1, feDev2, feDev3 } from "../interfaces/jobs.interface.js";
+import { beDev1, beDev2, beDev3, beDev4, beDev5, dba1, dba2, dba3, devSecOp1, devSecOp2, devSecOp3, devSecOp4, devSecOp5, feDev1, feDev2, feDev3, feDev4, feDev5 } from "../interfaces/jobs.interface.js";
 
 const user = JSON.parse(localStorage.getItem('user'));
 
@@ -22,19 +22,19 @@ console.log('jobProfile', jobProfile);
 let jobs = [];
 switch (jobProfile) {
     case 'front-end':
-        jobs = [feDev1, feDev2, feDev3];
+        jobs = [feDev1, feDev2, feDev3, feDev4, feDev5];
         break;
     case 'back-end':
-        jobs = [beDev1, beDev2, beDev3];
+        jobs = [beDev1, beDev2, beDev3, beDev4, beDev5];
         break;
     case 'devsecops':
-        jobs[devSecOp1, devSecOp2, devSecOp3];
+        jobs[devSecOp1, devSecOp2, devSecOp3, devSecOp4, devSecOp5];
         break;
     case 'dba':
-        jobs = [dba1, dba2, dba3];
+        jobs = [dba1, dba2, dba3, dba4, dba5];
         break;
     default:
-        jobs = [feDev1, devSecOp2, devSecOp3];
+        jobs = [feDev3, beDev5, devSecOp3, dba3, beDev1];
         break;
 }
 
@@ -47,9 +47,9 @@ document.addEventListener('DOMContentLoaded', () => {
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <input type="hidden" id="jobId" name="jobId" value="${job.id}">
+                    <img src="/src/img/${job.icon}" style="with:80px; height:80px" alt="${job.icon}">
                     <h5 class="card-title">${job.title}</h5>
                     <p class="card-text">${job.description}</p>
-                    <input type="submit" value="More info">
                 </div>
             </div>
         </form>
